12d6a268646351cb4d8ee84d9219904e53a63ad2
Frozenheart32 <alexroid32@gmail.com>
2022-04-05 00:27:48 +0300

    Added method injection example

---
+++ Assets/Scripts/HPBar.cs
@@ -0,0 +1,29 @@
+using DefaultNamespace;
+using UnityEngine;
+using Zenject;
+
+public class HPBar : MonoBehaviour
+{
+    private PlayerController _playerController;
+    
+    //Для большего контроля и понимания можно использовать инжекцию методов.
+    //Своего рода аналог конструктора для монобехов.
+    //Требование: все входные данные должны быть зарегистрированы в каком-нить MonoInstaller
+    [Inject]
+    private void Constructor(PlayerController playerController)
+    {
+        _playerController = playerController;
+    }
+
+    private void Start()
+    {
+        if (_playerController != null)
+        {
+            Debug.Log("HPBar: игрок получен");
+        }
+        else
+        {
+            Debug.Log("HPBar: игрок потерян");
+        }
+    }
+}
